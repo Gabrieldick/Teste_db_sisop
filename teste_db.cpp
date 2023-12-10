5beeb259ed73ef664992cc722b7fdba94a5b8a91
Gabriel_Dick <gabrieldick036@gmail.com>
2023-12-10 10:59:52 -0300

    Working Database

--- teste_db.cpp
+++ teste_db.cpp
@@ -4,75 +4,139 @@
 #include <string>
 #include <vector>
 #include <unordered_set>
+#include <iterator>
 
-void read_file(const std::string& filename) {
+struct User{
+		std::string username;
+		int userId;
+	};
+
+class UserInfo {
+		private:
+			User user;
+			int activeSessions;
+			std::unordered_set<int> followers;
+
+        public:
+            int getId() const {
+                return this->user.userId;
+            }
+
+            const std::string getUsername() const {
+                return user.username;
+            }
+
+            const std::unordered_set<int> getFollowers() const {
+                return this->followers;
+            }
+
+
+			UserInfo(int userId, std::string username, std::unordered_set<int> followers){
+                this->user.username = username;
+                this->user.userId = userId;
+                this->followers = followers;
+                this->activeSessions = 0;
+            }
+	};
+
+// Função para converter uma string para um unordered_set de int
+std::unordered_set<int> stringParaUnorderedSet(const std::string& input) {
+    std::unordered_set<int> result;
+
+    std::istringstream iss(input);
+    int elemento;
+    while (iss >> elemento) {
+        result.insert(elemento);
+        iss.ignore(1, ',');
+    }
+
+    return result;
+}
+
+// Função para converter um unordered_set de int para uma string
+std::string unorderedSetParaString(const std::unordered_set<int>& mySet) {
+    std::ostringstream oss;
+    for (const auto& elemento : mySet) {
+        oss << elemento << ",";
+    }
+    if (!mySet.empty()) {
+        oss.seekp(-1, oss.cur);
+    }
+    return oss.str();
+}
+
+// Função para ler um arquivo e retornar um vetor de UserInfo
+std::vector<UserInfo> read_file(const std::string& filename) {
     std::ifstream file(filename);
+    std::vector<UserInfo> usuarios;
 
     if (!file.is_open()) {
         std::cerr << "Erro ao abrir o arquivo: " << filename << std::endl;
-        return;
+        return usuarios;  // Retorna vetor vazio em caso de erro
     }
 
     std::string linha;
 
     while (std::getline(file, linha)) {
-        std::vector<std::string> palavras;  // Array de palavras
-        std::stringstream ss(linha);
-        std::string palavra;
+        std::string username;
+        std::string followers_str;
+        int id;
 
-        while (std::getline(ss, palavra, ';')) {
-            palavras.push_back(palavra);
-        }
+        std::istringstream ss(linha);
+        std::getline(ss, username, ';');
+        std::getline(ss, followers_str, ';');
+        ss >> id;
 
-        // Agora, palavras contém o array de palavras da linha, separadas por ';'
+        std::unordered_set<int> followers = stringParaUnorderedSet(followers_str);
 
-        // Exemplo: exibindo as palavras
-        std::cout << "Palavras na linha:" << std::endl;
-        for (const auto& p : palavras) {
-            std::cout << p << " palavra";
-        }
-        std::cout << std::endl;
+        // Criar UserInfo e adicionar ao vetor
+        usuarios.push_back(UserInfo(id, username, followers));
+    }
+
+    file.close();
+
+    return usuarios;
+}
+
+std::string format_data(const UserInfo& user) {
+    std::ostringstream formatted_data;
+    user.getFollowers();
+    formatted_data << user.getUsername() << ";" << unorderedSetParaString(user.getFollowers()) << ";" << user.getId();
+    return formatted_data.str();
+}
+
+// Função para escrever um vetor de UserInfo em um arquivo
+void write_file(const std::string& filename, const std::vector<UserInfo>& users) {
+    std::ofstream file(filename);
+
+    if (!file.is_open()) {
+        std::cerr << "Erro ao abrir o arquivo para escrita: " << filename << std::endl;
+        return;
+    }
+
+    for (const auto& user : users) {
+        std::string formatted_data = format_data(user);
+        file << formatted_data << std::endl;
     }
 
     file.close();
 }
 
+int main() {
 
+    // Exemplo de uso da função read_file
+    std::vector<UserInfo> usuarios = read_file("database.txt");
 
-std::string format_data(const std::string& username, const std::unordered_set<int>& followers, int id) {
-    std::ostringstream formatted_data;
-
-    // Adiciona o username
-    formatted_data << username << ";";
-
-    // Adiciona os seguidores separados por ","
-    for (auto it = followers.begin(); it != followers.end(); ++it) {
-        formatted_data << *it;
-        if (std::next(it) != followers.end()) {
-            formatted_data << ",";
+    // Exemplo de exibição dos dados lidos
+    std::cout << "Dados lidos do arquivo:" << std::endl;
+    for (const auto& users : usuarios) {
+        std::cout << "Username: " << users.getUsername() << " - Seguidores: ";
+        for (const auto& follower : users.getFollowers()) {
+            std::cout << follower << ",";
         }
+        std::cout << " - ID: " << users.getId() << std::endl;
     }
 
-    // Adiciona o ID
-    formatted_data << ";" << id;
 
-    return formatted_data.str();
-}
-
-int main() {
-    // Exemplo de uso da função read_file
-    //read_file("database.txt");  // Substitua "arquivo.txt" pelo nome do seu arquivo
-
-
-    // Exemplo de uso da função format_data
-    std::string username = "usuario1";
-    std::unordered_set<int> followers = {1, 2, 3, 4, 5, 6};
-    int id = 15;  // Substitua pelo valor real do ID
-
-    std::string formatted_string = format_data(username, followers, id);
-
-    // Exibe o resultado formatado
-    std::cout << "Formato da string: " << formatted_string << std::endl;
-    
     return 0;
 }
